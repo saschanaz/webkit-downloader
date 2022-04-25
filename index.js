5a298530f29c930fadad54fe1fce55e11a37a3be
Kagami Sascha Rosylight <saschanaz@outlook.com>
2022-04-25 18:47:24 +0200

    fix: use identifier instead of revision

--- index.js
+++ index.js
@@ -3,20 +3,22 @@ import unzipper from "unzipper";
 import { fetchAsJson, getBuilds } from "./lib.js";
 
 const { builds } = await getBuilds(27 /* WinCairo-64-bit-WKL-Release-Build */, [
-  "revision",
+  "identifier",
 ]);
 
-const latestCompleteBuild = builds.find((build) => build.complete);
-
-const { properties } = latestCompleteBuild;
-const [revision] = properties.revision;
-
-console.log(`Fetching and unzipping ${revision}.zip...`);
-const binary = await fetch(
-  `https://s3-us-west-2.amazonaws.com/archives.webkit.org/wincairo-x86_64-release/${revision}.zip`
+const latestSuccessfulBuild = builds.find(
+  (build) => build.state_string === "build successful"
 );
 
-binary.body.pipe(unzipper.Extract({ path: `downloads/${revision}` }));
+const { properties } = latestSuccessfulBuild;
+const [identifier] = properties.identifier;
+
+console.log(`Fetching and unzipping ${identifier}.zip...`);
+const binary = await fetch(
+  `https://s3-us-west-2.amazonaws.com/archives.webkit.org/wincairo-x86_64-release/${identifier}.zip`
+);
+
+binary.body.pipe(unzipper.Extract({ path: `downloads/${identifier}` }));
 
 const releases = await fetchAsJson(
   "https://api.github.com/repos/WebKitForWindows/WebKitRequirements/releases"
@@ -30,4 +32,4 @@ const assetWin64 = latestAssets.find((asset) => asset.name.includes("Win64"));
 
 const assetBinary = await fetch(assetWin64.browser_download_url);
 
-assetBinary.body.pipe(unzipper.Extract({ path: `downloads/${revision}` }));
+assetBinary.body.pipe(unzipper.Extract({ path: `downloads/${identifier}` }));
